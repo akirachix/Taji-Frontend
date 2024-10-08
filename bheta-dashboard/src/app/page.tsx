fae57e5b8800db3721e6f0bc821bfa2244a2d575
mercy-kantai <kantaiemilymercy@gmail.com>
2024-10-08 14:55:47 +0300

    added report pharmacy feature

--- bheta-dashboard/src/app/page.tsx
+++ bheta-dashboard/src/app/page.tsx
@@ -1,9 +1,10 @@
+import PharmacyReportForm from "./(pwa)/pwa/Pharmacy";
 import PharmacyHospitalFinder from "./(pwa)/pwa/map";
 
 export default function Home() {
   return (
     <div>
-      <PharmacyHospitalFinder/>
+      <PharmacyReportForm/>
     </div>
   );
 }
