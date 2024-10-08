6052e9e8de2360b7c44d6a288fa8ddc499e872e3
mercy-kantai <kantaiemilymercy@gmail.com>
2024-10-08 15:04:05 +0300

    added report pharmacy feature

--- bheta-dashboard/src/app/(pwa)/pwa/Pharmacy/index.tsx
+++ bheta-dashboard/src/app/(pwa)/pwa/Pharmacy/index.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import Image from 'next/image';
 import { useForm, SubmitHandler } from 'react-hook-form';
 import Navbar from '../Navbar';
